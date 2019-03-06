485799c6455602fb61cec2bb9b8ffcf98dfedbe8
Lena Hamilton 13 <hamiltonlena21@gmail.com>
2019-03-05 17:29:37 -0700

    mostly js i had help with

---
+++ assets/javascript/game.js
@@ -0,0 +1,93 @@
+// Selects a random number between 19 - 120
+var Random=Math.floor(Math.random()*101+19)
+   
+$('#computerPoints').text(computerPoints);
+
+//random numbers for each jewel between 1 - 12
+var num1 = Math.floor(Math.random()*11+1)
+var num2 = Math.floor(Math.random()*11+1)
+var num3 = Math.floor(Math.random()*11+1)
+var num4 = Math.floor(Math.random()*11+1)
+   
+var currentPoints = 0; 
+var winsCounter = 0;
+var lossCounter = 0;
+
+
+$('#winCounter').text(winCounter);
+$('#lossCounter').text(lossCounter);
+
+
+//===================================================
+//main game
+
+function reset() {
+    Random=Math.floor(Math.random()*101+19);
+    $('#computerPoints').text(Random);
+
+    num1 = Math.floor(Math.random()*11+1);
+    num2 = Math.floor(Math.random()*11+1);
+    num3 = Math.floor(Math.random()*11+1);
+    num4 = Math.floor(Math.random()*11+1);
+    currentPoints = 0;
+    $('#currentPoints').text(currentPoints);
+     } 
+
+ function win() {
+    winCounter++; 
+    $('#winCounter').text(winCounter);
+    reset();
+    }
+
+function loss() {
+    lossCounter++;
+    $('#lossCounter').text(lossCounter);
+    reset();
+    }
+
+//clickys for jewels ==================================
+
+
+$('#amathystBall').on ('click', function(){
+    currentPoints = currentPoints + num1;
+    $('#currentPoints').text(currentPoints);
+
+    if (currentPoints == Random){
+        win();
+    } else (currentPoints > Random); {
+        loss();
+        };   
+    }); 
+
+$('#bigCrystal').on ('click', function(){
+    currentPoints = currentPoints + num2;
+    $('#currentPoints').text(currentPoints);
+
+    if (currentPoints == Random){
+        win();
+    } else (currentPoints > Random); {
+        loss();
+          } 
+    });
+
+$('#pearAmathyst').on ('click', function(){
+    currentPoints = currentPoints + num3;
+    $('#currentPoints').text(currentPoints);
+            
+    if (currentPoints == Random){
+        win();
+    } else (currentPoints > Random); {
+        loss();
+        } 
+    });
+
+$('#3crystals').on ('click', function(){
+    currentPoints = currentPoints + num4;
+    $('#currentPoints').text(currentPoints);
+        
+    if (currentPoints == Random){
+        win();
+    } else (currentPoints > Random); {
+        loss(); {
+        }
+    }});  
